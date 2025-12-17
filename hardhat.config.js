d0fe72662b7ef48f9c35d45ad4aaec501f60eab8
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-12-17 18:50:18 +0530

    dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+
+const { PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+networks: {
+    "base-sepolia": {
+      url: "https://sepolia.base.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+}
 };
